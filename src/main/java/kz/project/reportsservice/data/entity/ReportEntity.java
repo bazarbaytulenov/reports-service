c43d29dc9b3b0b1c308c85309d942b63c4958c89
bazarbay <bazarbay.tulenov@gmail.com>
2023-12-01 00:04:58 +0600

    bug fix

--- src/main/java/kz/project/reportsservice/data/entity/ReportEntity.java
+++ src/main/java/kz/project/reportsservice/data/entity/ReportEntity.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name="report",schema="report_schema")
+@Table(name="report")
 @Data
 @NoArgsConstructor
 public class ReportEntity {
